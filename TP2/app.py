dc81beb8068be8c281efb9ddb443f027c443f23d
Tassiana Bastos <tassianalb94@gmail.com>
2026-08-24 15:28:18 +0100

    Update app.py

--- TP2/app.py
+++ TP2/app.py
@@ -1,1 +1,8 @@
-
+"""
+1. Importância da Visualização de Dados
+No contexto da COVID-19; a visualizacao de dados pode auxiliar na identificacao da evolucao dos casos e dos obitos ao longo do tempo, permitindo observar periodos de aumento ou reducao da doenca e diferencas entre 
+estados, regioes e municipios. 
+Para os gestores de saude publica, essas informacoes sao importantes para apoiar a tomada de decisoes, como o planejamento de recursos, a identificacao de regioes que necessitam de maior atencao e o acompanhamento da 
+evolucao da situacao epidemiologica.
+Para a populacao em geral, graficos e mapas podem facilitar a compreensao da situacao da pandemia e permitir o acompanhamento de informacoes relevantes de forma mais clara.
+"""
